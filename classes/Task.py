1fcfaec78c896e9f9913d4e643bd6f31c44d956a
RiverRobins <rivergrahamrobins@gmail.com>
2020-09-14 16:40:23 -0500

    first commit

---
+++ classes/Task.py
@@ -0,0 +1,23 @@
+import datetime
+from tkinter import *
+
+class Task:
+    def __init__(self, title=None, description=None, deadline=datetime.datetime(year=9999, month=12, day=31, hour=0, minute=0, second=0, microsecond=0), sql=None, *args):
+        if(sql is None):
+            self.title = title
+            self.created = datetime.datetime.now()
+            self.status = "incomplete"
+            self.description = description
+            self.deadline = deadline
+        else:
+            self.id = sql[0]
+            self.title = sql[1]
+            self.created = sql[2]
+            self.status = sql[3]
+            self.description = sql[4] if sql[4] != "NULL" else None
+            self.deadline = sql[5] if sql[5] != "NULL" else None
+        self.dependencies = args
+
+    def display(self, base):
+        mainframe = Frame(base)
+
