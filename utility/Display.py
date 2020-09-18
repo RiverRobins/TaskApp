9d9a08d5e87ad771607597db4ae747e636521ff6
RiverRobins <rivergrahamrobins@gmail.com>
2020-09-18 14:48:23 -0500

    added mysql

--- utility/Display.py
+++ utility/Display.py
@@ -7,11 +7,16 @@ def daily_card(base, task):
     mainframe.pack()
 
     title = Label(mainframe, text=task.title)
+    title.pack()
     subtitle = Label(mainframe, text=f"{task.status}, due by {format_time(task.deadline[10, -1])}" if(task.status == "incomplete") else task.status)
+    subtitle.pack()
     description = Label(mainframe, text=task.description)
+    description.pack()
 
     button_done = Button(mainframe, text="Done")
+    button_done.pack()
     button_failed = Button(mainframe, text="Incompleted")
+    button_failed.pack()
 
 
 def format_time(n):
